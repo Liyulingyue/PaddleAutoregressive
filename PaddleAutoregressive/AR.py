773ccc6cbc5146503c0e11f923eb130c18de4e47
liyulingyue <852433440@qq.com>
2023-01-28 09:13:09 +0800

    update

---
+++ PaddleAutoregressive/AR.py
@@ -0,0 +1,15 @@
+from Autoregressive import Autoregressive
+import paddle
+
+class AR(paddle.nn.Layer):
+    def __init__(self, y_features):
+        # y_features should be a int nunber
+        # x_features should be a list of int
+        # e_features should be a int nunber
+        super(AR, self).__init__()
+        self.y_features = y_features
+        self.Autoregressive = Autoregressive(y_features, [], 0)
+
+    def forward(self, *inputs):
+        output = self.Autoregressive(inputs[0])
+        return output
